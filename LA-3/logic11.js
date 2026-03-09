1ad16b4af250def2aa06519fdf88f8e98cba7c70
Nihit-Garg <nihitgarg2005@gmail.com>
2026-03-09 20:08:05 +0530

    feat: codes for program 5 to 16 added

---
+++ LA-3/logic11.js
@@ -0,0 +1,8 @@
+function calculateSI(principal, rate, time) {
+    // Mathematical calculation in external function
+    // Formula implementation: SI = (P x R x T) / 100
+    var simpleInterest = (principal * rate * time) / 100;
+
+    // Returning calculated result
+    return simpleInterest;
+}
